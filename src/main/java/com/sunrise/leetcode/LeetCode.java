3c3b5f2b31c7e78d367d870d54b34b3b826e490c
lzhaoyang <575563079@qq.com>
2019-05-22 00:32:06 +0800

    init the repo

---
+++ src/main/java/com/sunrise/leetcode/LeetCode.java
@@ -0,0 +1,13 @@
+package com.sunrise.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class LeetCode
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome to leetcode!" );
+    }
+}
---
+++ src/test/java/com/sunrise/leetcode/LeetCodeTest.java
@@ -0,0 +1,20 @@
+package com.sunrise.leetcode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LeetCodeTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
