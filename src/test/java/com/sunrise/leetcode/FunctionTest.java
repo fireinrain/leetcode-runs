9a5c62def02a77106fa96cb86c0e6b79b53f30c6
lzhaoyang <575563079@qq.com>
2019-05-23 17:55:10 +0800

    测试专用

---
+++ src/test/java/com/sunrise/leetcode/FunctionTest.java
@@ -0,0 +1,17 @@
+package com.sunrise.leetcode;
+
+/**
+ * @description:
+ * @date: 2019/5/23 17:52
+ * @auther: sunrise
+ * @Gmail: liu575563079@gmail.com
+ */
+public class FunctionTest {
+    public static void main(String[] args) {
+        int x = 12345;
+        while (x>0){
+            x = x/10;
+            System.out.println(x);
+        }
+    }
+}
